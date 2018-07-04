c1728b7942cffaad07f01bcafa13edd2118039aa
Tristan Brown <tgbrown@stanford.edu>
2018-07-04 19:06:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+ var phrases = [];
+ for (var i = 0; i < musicians.length; i++) {
+    phrases.push(musicians[i] + " plays " + instruments[i]);
+  }
+ return phrases;
+}
+
+function johnLennonFacts(facts) {
+  var results = [];
+  var i = 0;
+  while (i < facts.length) {
+    results.push(facts[i] + "!!!");
+    count++;
+  }
+  return results;
+}
