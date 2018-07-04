c06afabef6ec899b66fc4be07c416cd4c93ebb65
Tristan Brown <tgbrown@stanford.edu>
2018-07-04 19:22:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,10 +18,9 @@ function johnLennonFacts(facts) {
 
 function iLoveTheBeatles(number) {
   var array = [];
-  var i = 0;
   do {
     array.push("I love the Beatles!");
-    i++;
+    number++;
   }
   while (i<15);
   return array 
